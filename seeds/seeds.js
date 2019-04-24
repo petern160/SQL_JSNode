059895ca88902e5eca85bb548bbee04117c77aa7
petern160 <peternguyen160@gmail.com>
2019-04-24 00:07:11 +0000

    completed

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,15 +1,16 @@
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('milestones').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('milestones').insert([
-        { description: 'went to the moon',
-        date_achieved: '1993-12-12'},
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('milestones').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('milestones').insert([
+
+//         { description: 'went to the moon',
+//         date_achieved: '1993-12-12'},
         
-         { description: 'won a gold medal'},
-        { description: 'best actor award'}
-      ]);
-    });
-};
+//          { description: 'won a gold medal'},
+//         { description: 'best actor award'}
+//       ]);
+//     });
+// };
